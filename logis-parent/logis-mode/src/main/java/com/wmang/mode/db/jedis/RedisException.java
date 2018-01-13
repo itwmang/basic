9b957fbaf948da4167d41f97232d92268bc360f9
wmang <itfast@yeah.net>
2018-01-13 11:28:33 +0800

    整理版maven项目.纯净版.配置mysql+mybatis,springmvc,hui.admin页面.

---
+++ logis-parent/logis-mode/src/main/java/com/wmang/mode/db/jedis/RedisException.java
@@ -0,0 +1,17 @@
+package com.wmang.mode.db.jedis;
+
+import com.wmang.mode.exception.BaseException;
+
+public class RedisException extends BaseException {
+
+	protected RedisException(String defineCode) {
+		super(defineCode);
+	}
+
+	/**
+	 * @Fields serialVersionUID :
+	 */
+	private static final long serialVersionUID = 3988229133456964977L;
+
+
+}
